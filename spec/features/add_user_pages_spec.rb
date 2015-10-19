4a9ad17412a5ae4d36e27ce795cabee2df5e6437
Kendra Ash & Vaughn Dice <student@epicodus.com>
2015-10-19 11:26:39 -0700

    add error scenarios to add_user integration spec

--- spec/features/add_user_pages_spec.rb
+++ spec/features/add_user_pages_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe 'the add a user process' do
+  password = 'password'
   it 'can add a user' do
-    password = 'password'
 
     visit users_path
     click_on 'Sign Up'
@@ -12,5 +12,22 @@ describe 'the add a user process' do
     fill_in 'Password confirmation', :with => password
     click_on 'Sign Up'
     expect(page).to have_content 'Bernie Sanders'
+  end
+
+  it 'displays errors when the form password and password confirmation do not match' do
+    visit users_path
+    click_on 'Sign Up'
+    fill_in 'Username', :with => 'Bernie Sanders'
+    fill_in 'Email', :with => 'bernie@sanders.com'
+    fill_in 'Password', :with => password
+    click_on 'Sign Up'
+    expect(page).to have_content "There was a problem creating your account"
+  end
+
+  it 'displays errors when the form is incomplete' do
+    visit users_path
+    click_on 'Sign Up'
+    click_on 'Sign Up'
+    expect(page).to have_content "errors"
   end
 end
